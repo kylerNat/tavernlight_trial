66968a43c70715c7e583b26d8105878893b3da3d
Kyler <kylernat@gmail.com>
2024-04-18 08:00:17 -0700

    Q4

--- q4.cpp
+++ q4.cpp
@@ -1,8 +1,11 @@
 void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
 {
+    /* I have chosen to allocate a player on the stack to avoid using new at all and remove any chance of a leak.
+       I think this is the simplest and cleanest solution since it avoids any dynamic memory allocation, but alternative way to fix the leak would be to use smart pointers */
+    Player tempPlayer(nullptr);
     Player* player = g_game.getPlayerByName(recipient);
     if (!player) {
-        player = new Player(nullptr);
+        player = &tempPlayer;
         if (!IOLoginData::loadPlayerByName(player, recipient)) {
             return;
         }
